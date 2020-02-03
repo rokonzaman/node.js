6fde7ffea41772d3079f99d228922f5ce3f13a44
Md. Roknuzzamann <54236147+rokonzaman@users.noreply.github.com>
2020-02-03 17:58:33 +0600

    Update App.js
    node.js application pipeline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is test page
+          I have successfull completed my node.js application pipeline.
         </p>
         <a
           className="App-link"
